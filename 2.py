188714f3c6ff8c3ccbc2ad77a63de13dfd3e1a50
tungyuping1228 <80875803+tungyuping1228@users.noreply.github.com>
2021-03-18 15:47:17 +0800

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,14 @@
+import sys
+print("Python version:", sys.version)
+import pandas as pd
+print("pandas version;", pd._version_)
+import matplotlib
+print("matplotlib version:", matplotlib._version_)
+import numpy as np
+print("NumPy version:", np._version_)
+import scipy as sp
+print("SciPy version:", sp._version_)
+import IPython
+print("IPython version:", IPython._version_)
+import sklearn
+print("scikit-learn version:", sklearn._version_)
